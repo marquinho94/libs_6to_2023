f381e245ea074752ace37ab101444d56f36918ce
marquinho94 <marcoalanmiranda@gmail.com>
2023-06-25 12:39:58 -0300

    cambios menores

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,9 @@
 
 
 
-input A,B,C,D,E,F,G;
+
 void setup() {
 
-  A.setInput(2);
-  B.setInput(3);
-  C.setInput(4);
-  D.setInput(5);
-  E.setInput(6);
-  F.setInput(7);
-  G.setInput(8);
 
 
 
@@ -27,15 +20,6 @@ void loop() {
 
 
 
-  int a,b,c,d,e,f,g;
-  a=A.getInput();
-  b=B.getInput();
-  c=C.getInput();
-  d=D.getInput();
-  e=E.getInput();
-  f=F.getInput();
-  g=G.getInput();
 
-  Serial.println("hola");
 }
 
