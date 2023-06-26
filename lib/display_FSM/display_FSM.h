d8c67cd29f082956e25ec94dd6226a0f1a00f409
marquinho94 <marcoalanmiranda@gmail.com>
2023-06-26 12:59:16 -0300

    cambios menores

--- lib/display_FSM/display_FSM.h
+++ lib/display_FSM/display_FSM.h
@@ -9,7 +9,7 @@
     #include <LiquidCrystal.h>
     #endif 
 
-void displaySet (int cols, int rows,const int rs = 12,const int en = 11,const int d4 = 5,const int d5 = 4,const int d6 = 3,const int d7 = 2);
+void displaySet (int cols, int rows ,const int rs = 12,const int en = 11,const int d4 = 5,const int d5 = 4,const int d6 = 3,const int d7 = 2);
 
 
 
